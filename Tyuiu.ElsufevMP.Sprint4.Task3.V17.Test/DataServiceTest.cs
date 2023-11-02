0ef1f2d263533e1f0e3ee74c140206b952278920
Tyuiu.ElsufevMP@outlook.com <mihal.els.32@gmail.com>
2023-11-02 19:55:21 +0500

    Task3

---
+++ Tyuiu.ElsufevMP.Sprint4.Task3.V17.Test/DataServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+using Tyuiu.ElsufevMP.Sprint4.Task3.V17.Lib;
+
+namespace Tyuiu.ElsufevMP.Sprint4.Task3.V17.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int[,] m = new int[5, 5]
+            {
+                {6, 4, 2, 2, 1 },
+                {3, 6, 5, 4, 1 },
+                {5, 2, 3, 1, 6 },
+                {8, 8, 4, 5, 3 },
+                {7, 4, 5, 1, 6 }
+            };
+
+            int res = ds.Calculate(m);
+            int wait = 13;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.ElsufevMP.Sprint4.Task3.V17/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.ElsufevMP.Sprint4.Task3.V17.Lib;
+
+namespace Tyuiu.ElsufevMP.Sprint4.Task3.V17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #4 | Выполнил: Елсуфьев М. П. | ИИПб-23-2";
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #4                                                               *");
+            Console.WriteLine("* Тема: Обработка структурных типов                                       *");
+            Console.WriteLine("* Задание #3                                                              *");
+            Console.WriteLine("* Вариант #17                                                             *");
+            Console.WriteLine("* Выполнил: Елсуфьев Михаил Павлович | ИИПб-23-2                          *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Найдите сумму элементов в четвертом столбце массива.                    *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ                                                         *");
+            Console.WriteLine("***************************************************************************");
+
+            int[,] arr = new int[5, 5]{
+                {6, 4, 2, 2, 1 },
+                {3, 6, 5, 4, 1 },
+                {5, 2, 3, 1, 6 },
+                {8, 8, 4, 5, 3 },
+                {7, 4, 5, 1, 6 } };
+
+            int rows = arr.GetUpperBound(0) + 1;
+            int colums = arr.Length / rows;
+
+            Console.WriteLine("Массив: ");
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < colums; j++)
+                {
+                    Console.Write($"{arr[i, j]} \t");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int res = ds.Calculate(arr);
+
+            Console.WriteLine("Сумма элементов в 4-ом столбце = " + res);
+
+            Console.ReadKey();
+        }
+    }
+}
